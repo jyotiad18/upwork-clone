dbcd5795cf779b05bfcaa247502e55374a8cf740
Jyoti Adhikari <jyotiad18@gmail.com>
2020-08-06 01:44:54 +0200

    added the feeds

--- src/Feeds.js
+++ src/Feeds.js
@@ -1,10 +1,94 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './Feeds.css';
+import IconButton from "@material-ui/core/IconButton";
+import MoreHorizRoundedIcon from '@material-ui/icons/MoreHorizRounded';
+import Button from '@material-ui/core/Button';
+import ThumbDownRoundedIcon from '@material-ui/icons/ThumbDownRounded';
+import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import StarIcon from '@material-ui/icons/Star';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { initData } from './init';
 
-function Feeds() {
+function Feeds() {	
+	const [feeds, setFeeds] = useState(initData);
+
 	return (
 		<div className="feeds">
-			
+			<section className="feeds__sectiontop">
+				<h1 className="feeds__myFeed">My Feed</h1>
+				<IconButton className="feeds__iconButton">
+					<MoreHorizRoundedIcon						
+					></MoreHorizRoundedIcon>
+				</IconButton>				
+			</section>
+			{
+				feeds?.map((feed) => (
+					<section className="feeds__detail">	
+						<div className="feeds__header">
+							<h1 className="feeds__headerTitle">{feed.title}</h1>
+							<div className="feeds__detailIcons">
+								<IconButton className="feeds__iconButton">
+									<ThumbDownRoundedIcon
+									></ThumbDownRoundedIcon>
+								</IconButton>
+								<IconButton className="feeds__iconButton">
+									<FavoriteBorderRoundedIcon
+									></FavoriteBorderRoundedIcon>
+								</IconButton>
+							</div>
+						</div>	
+						<div className="feeds__timeprice">
+							<div className="feeds__hourly">Hourly: {feed.hourly}</div>
+							<div className="feeds__duration">
+								<h6>{' - '}</h6>
+								<h6>{feed.level}</h6>
+								<h6>{' - Est. Time: '}</h6>								
+								<h6>{' Less than '}</h6>
+								<h6>{feed.Duration}</h6>
+								<h6>{','}</h6>
+								<h6>{feed.workhour}</h6>
+								<h6>{' - Posted'}</h6>
+								<h6>{feed.posted}</h6>			
+							</div>	
+						</div>	
+						<div className="feeds__description">
+							{feed.description}
+						</div>
+						<div className="feeds__languages">
+							{feed.language?.map(l => (
+								<div className="feeds__language">{l}</div>
+							))
+							}
+						</div>
+						<div className="feeds__timeprice">
+							{'Hourly: '}<div className="feeds__hourly">{feed.proposed}</div> 
+						</div>
+						<div className="feeds__verified">
+							<div className="feeds__rates">
+								<VerifiedUserIcon className="feeds__verifiedIcon"></VerifiedUserIcon>
+								<h6>Payment Verified</h6>
+								<div className="feeds__stars">
+									<StarIcon className="feeds__star" fontSize="small"></StarIcon>
+									<StarIcon className="feeds__star" fontSize="small"></StarIcon>
+									<StarIcon className="feeds__star" fontSize="small"></StarIcon>
+									<StarIcon className="feeds__star" fontSize="small"></StarIcon>
+									<StarIcon className="feeds__star" fontSize="small"></StarIcon>
+								</div>
+							</div>
+							<div className="feeds__place">
+								<LocationOnIcon></LocationOnIcon>
+								<h6>{feed.place}</h6>
+							</div>
+						</div>
+					</section>
+				))
+			}
+			<section className="feeds__sectionbottom">
+				<Button variant="contained" className="feeds__button" disableRipple>
+					Load More Jobs
+				</Button>
+			</section>
 		</div>
 	)
 }
---
+++ src/init.js
@@ -0,0 +1,45 @@
+export const initData = [{
+	'title': 'Portal Development - ASPNet Zero and Angular',
+	'hourly': '$10.00 - $15.00',
+	'Duration': '3 - 6 months ',
+	'language': ['C#', 'JavaScript', 'Microsoft SQL Server'],
+	'posted': '52 minute ago',
+	'proposed': ' 10 - 15',
+	'description': 'We are currently developing our client portal using ASPNet Zero with Angular front end.  We need to add a development resource to our team to build out more features and BI widgets in the portal.The first phase of this engagement will focus on the creating of individual BI widgets for use on the customizable dashboard page of the portal.',
+	'level': 'Export',
+	'place': 'United State',
+	'workhour': '30+ hrs/week '
+}, {
+		'title': 'Uploader Application Build in C#',
+		'hourly': '$20.00 - $30.00',
+		'Duration': '1 - 2 months ',
+		'language': ['C#', 'JavaScript', 'Microsoft SQL Server', 'Angular'],
+		'posted': '52 minute ago',
+		'proposed': ' 5 - 7',
+		'description': 'We are looking for someone to take on a C# project.We have five different laboratories throughout the country.They all need to have an uploader that takes files from a local folder(windows) and pushes them to our Amazon S3 bucket.We have a basic uploader that were using right now, and the repo can be found Each application needs to have its own credentials.',
+		'level': 'Export',
+		'place': 'United State',
+		'workhour': '10+ hrs/week '
+	}, {
+		'title': 'Portal Development - ASPNet Zero and Angular',
+		'hourly': '$10.00 - $15.00',
+		'Duration': '3 - 6 months ',
+		'language': ['C#', 'JavaScript', 'Microsoft SQL Server'],
+		'posted': '52 minute ago',
+		'proposed': ' 10 - 15',
+		'description': 'We are currently developing our client portal using ASPNet Zero with Angular front end.  We need to add a development resource to our team to build out more features and BI widgets in the portal.The first phase of this engagement will focus on the creating of individual BI widgets for use on the customizable dashboard page of the portal.',
+		'level': 'Export',
+		'place': 'United State',
+		'workhour': '30+ hrs/week '
+	}, {
+		'title': 'Uploader Application Build in C#',
+		'hourly': '$20.00 - $30.00',
+		'Duration': '1 - 2 months ',
+		'language': ['C#', 'JavaScript', 'Microsoft SQL Server', 'Angular'],
+		'posted': '52 minute ago',
+		'proposed': ' 5 - 7',
+		'description': 'We are looking for someone to take on a C# project.We have five different laboratories throughout the country.They all need to have an uploader that takes files from a local folder(windows) and pushes them to our Amazon S3 bucket.We have a basic uploader that were using right now, and the repo can be found Each application needs to have its own credentials.',
+		'level': 'Export',
+		'place': 'United State',
+		'workhour': '10+ hrs/week '
+	}]
